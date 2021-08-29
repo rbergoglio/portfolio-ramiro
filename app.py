3817c294cf706a8a5fa997ccc961f7b781a35c90
Ramiro Bergoglio <ramiro.bergoglio@gmail.com>
2021-08-29 02:08:22 -0300

    added flashcards

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, abort,jsonify
 
 app = Flask(__name__)
 
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+#.venv\scripts\activate        
 #$env:FLASK_ENV = "development"
 #flask run
+
+from model import db
 
 counter = 0
 
 @app.route("/")
 def home():
-    return render_template("home.html", message="hola")
+    return render_template("home.html", cards=db)
 
 
 @app.route("/date")
 def date():
     return "<p>Hello, World!</p>" + str(datetime.now())
 
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+    except IndexError:
+        abort(404)
 
-@app.route("/count-views")
-def count_demo():
-    global counter
-    counter += 1
-    return "<p>This page was visited</p>" + str(counter)
+
+
+@app.route("/api/card")
+def api_card_list():
+    return jsonify(db)
+
+@app.route("/api/card/<int:index>")
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
---
+++ model.py
@@ -0,0 +1,9 @@
+#todo: use sqlalchemy instead
+
+import json
+
+def load_db():
+    with open("flashcards_db.json") as f:
+        return json.load(f)
+
+db = load_db()

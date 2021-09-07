becbd9c3ee0a6db1413b61fe64e2c63cf2e84ff6
Ramiro Bergoglio <ramiro.bergoglio@gmail.com>
2021-09-07 17:08:08 -0300

    add items to db

--- app.py
+++ app.py
@@ -9,7 +9,9 @@
 
 import pdb
 from datetime import datetime
-from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for, g
+
+import sqlite3
 
 app = Flask(__name__)
 
@@ -49,9 +51,20 @@ def api_card_detail(index):
 
 @app.route("/item/new",methods=["GET","POST"])
 def new_item():
+    conn = get_db()
+    c = conn.cursor()
     if request.method == "POST":
-       # pdb.set_trace()
-       #
+        # pdb.set_trace()
+        c.execute(""" INSERT INTO items (title, description, price, image, category_id, subcategory_id) VALUES (?,?,?,?,?,?) """,
+        (
+            request.form.get("title"),
+            request.form.get("description"),
+            float(request.form.get("price")),
+            "",
+            1,
+            1
+        )) 
+        conn.commit()
         return redirect(url_for('home'))
     else:
         return render_template("new_item.html")
@@ -67,3 +80,19 @@ def remove_card(index):
             return render_template("remove_card.html", card=db[index])
     except IndexError:
         abort(404)
+
+
+def get_db():
+    #hay que fijarse si ya existe una conexion en el contexto de la app "g", si no existe se crea
+    db = getattr(g,"_database", None)
+    if db is None:
+        db = g.database = sqlite3.connect("db/rbwebshop.db")
+    return db
+
+#este decorador hace que se ejecute cuando termina el contexto de la app.  cuando la request termina esta funcion va a cerrar la coneccion 
+@app.teardown_appcontext   
+def close_connection(exception):
+    db = getattr(g,"_database", None)
+    if db is not None:
+        db.close()
+
